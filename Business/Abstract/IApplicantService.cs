f9e2e36f1d7d5b0bdc645a777a5db8c941402ff3
abdullahyildirim <abdullahahmetyildirim@gmail.com>
2024-02-07 14:18:14 +0300

    first commit

---
+++ Business/Abstract/IApplicantService.cs
@@ -0,0 +1,9 @@
+﻿using Entities.Concrete;
+
+namespace Business.Abstract
+{
+    public interface IApplicantService
+    {
+        bool CheckPerson(Person person);
+    }
+}
---
+++ Business/Concrete/PersonManager.cs
@@ -0,0 +1,31 @@
+﻿using Entities.Concrete;
+using System.Collections.Generic;
+using Business.Abstract;
+using MernisServiceReference;
+
+namespace Business.Concrete
+{
+    public class PersonManager : IApplicantService
+    {
+        public void ApplyForMask(Person person)
+        {
+            
+        }
+
+        public List<Person> GetList()
+        {
+            return null;
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            KPSPublicSoapClient client=new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+
+            return client.TCKimlikNoDogrulaAsync(
+                new TCKimlikNoDogrulaRequest
+                (new TCKimlikNoDogrulaRequestBody(person.NationalIdentity,person.FirstName,person.LastName,person.DateOfBirthYear)))
+                .Result.Body.TCKimlikNoDogrulaResult;
+
+        }
+    }
+}
